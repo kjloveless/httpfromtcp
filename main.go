df8300d1aa34b221e11a3771ed45d2e2ce531b49
Kyle Loveless <kjloveless321@gmail.com>
2025-03-19 13:44:40 -0500

    print line by line

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
   "fmt"
   "io"
   "os"
+  "strings"
 )
 
 func main() {
@@ -13,11 +14,21 @@ func main() {
   }
 
   data := make([]byte, 8)
+  var line string
   for {
     _, err := file.Read(data)
     if err == io.EOF {
+      if len(line) > 0 {
+        fmt.Printf("read: %s\n", line)
+      }
       os.Exit(0)
     }
-    fmt.Printf("read: %s\n", data)
+
+    parts := strings.Split(string(data), "\n")
+    line += parts[0]
+    if len(parts) == 2 {
+      fmt.Printf("read: %s\n", line)
+      line = parts[1]
+    }
   }
 }
