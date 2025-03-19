6ac7d56e5d7f93b44460239c7574aceb70e152a3
Kyle Loveless <kjloveless321@gmail.com>
2025-03-19 10:37:20 -0500

    read 8 bytes at a time

--- main.go
+++ main.go
@@ -2,8 +2,22 @@ package main
 
 import (
   "fmt"
+  "io"
+  "os"
 )
 
 func main() {
-  fmt.Println("I hope I get the job!")
+  file, err := os.Open("messages.txt")
+  if err != nil {
+    os.Exit(1)
+  }
+
+  data := make([]byte, 8)
+  for {
+    _, err := file.Read(data)
+    if err == io.EOF {
+      os.Exit(0)
+    }
+    fmt.Printf("read: %s\n", data)
+  }
 }
