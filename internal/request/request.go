641e3d1a4723179feb6582666d4d9cd19eb6f9e1
Kyle Loveless <kjloveless321@gmail.com>
2025-03-24 16:04:34 -0500

    init air, begin parsing request-line

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,1 +1,34 @@
 package request
+
+import (
+  "io"
+  "log"
+  "strings"
+)
+
+type Request struct {
+  RequestLine RequestLine
+}
+
+type RequestLine struct {
+  HttpVersion   string
+  RequestTarget string
+  Method        string
+}
+
+func RequestFromReader(reader io.Reader) (*Request, error) {
+  data, err := io.ReadAll(reader)
+  if err != nil {
+    log.Fatalf("error: %v", err)
+  }
+
+  req := strings.Split(string(data), "\r\n")[0]
+  reqLine := strings.Split(req, " ")
+
+  log.Println(req)
+  return &Request{RequestLine{
+    HttpVersion:    strings.Split(reqLine[2], "HTTP/")[1],
+    RequestTarget:  reqLine[1],
+    Method:         reqLine[0],
+  }}, nil
+}
